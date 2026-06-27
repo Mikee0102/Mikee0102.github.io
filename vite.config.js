56c789c49fbd5fecd327235ac597c03854fec3f5
Mike <mv236648@gmail.com>
2026-06-26 22:26:29 -0600

    Agregando workflow para despliegue

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Mikee0102.github.io/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
